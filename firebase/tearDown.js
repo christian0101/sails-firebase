458cca60fa194e823433074f15de234f9093c719
JP Ventura <contact@jpventura.com>
2017-01-18 09:48:52 -0200

    :sparkles: Create firebase.tearDown

--- firebase/tearDown.js
+++ firebase/tearDown.js
@@ -22,14 +22,37 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-var Define = require('./define');
-var Drop = require('./drop');
-var RegisterApplication = require('./registerApplication');
-var TearDown = require('./tearDown');
 
-module.exports = {
-  'define': Define,
-  'drop': Drop,
-  'registerApplication': RegisterApplication,
-  'tearDown': TearDown
+var _ = require('lodash');
+var admin = require('firebase-admin');
+var Promise = require('bluebird');
+
+/**
+ * Fired when a model is unregistered, typically when the server
+ * is killed. Useful for tearing down remaining open applications,
+ * etc.
+ *
+ * @param  {String}  application (optional) The datastore to tear down. If not
+ *                               provided, all applications will be torn down.
+ * @return {Promise}             [description]
+ */
+var TearDown = function TearDown(application) {
+  try {
+    var apps = application ? [ application ] : _.values(_.mapValues(admin.apps, 'name_'));
+    var promises = [];
+
+    var onTearDown = function onTearDown() {
+      return null;
+    };
+
+    apps.forEach(function(identity) {
+      promises.push(admin.app(identity).delete());
+    });
+
+    return Promise.all(promises).then(onTearDown);
+  } catch (error) {
+    return Promise.reject(error);
+  }
 };
+
+module.exports = TearDown;
